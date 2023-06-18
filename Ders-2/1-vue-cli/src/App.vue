7857728bf7fcffd99599385f9b32abed9c875d8d
Enes Can Aydemir <enescan_aydemir@hotmail.com>
2023-06-18 16:35:05 +0300

    Ders-2 and vue-cli studies created

---
+++ Ders-2/1-vue-cli/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <app-header />
+  <div class="container">
+    <h3>CLI Uygulaması</h3>
+    <p>
+      Contents will be extracted and passed on to @vue/compiler-dom,
+      pre-compiled into JavaScript render functions, and attached to the
+      exported component as its render option
+    </p>
+  </div>
+  <!-- <CounterItem></CounterItem> -->
+  <!-- <bu-bir-sayac></bu-bir-sayac> -->
+  <CounterItem />
+</template>
+
+<script>
+import CounterItem from "@/components/CounterItem";
+
+export default {
+  components: {
+    CounterItem
+    // "bu-bir-sayac": CounterItem, //key-value; key=değiştirilebilir. value ise gelen value ismi olmalı.
+  },
+};
+</script>
---
+++ Ders-2/1-vue-cli/src/components/appHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>Vue Works</header>
+</template>
+
+<style>
+header {
+  background-color: rebeccapurple;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+  border-bottom: 2px solid rgb(5, 5, 5);
+}
+</style>
